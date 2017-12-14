3ae5ee68dc5d7a5bdebba319ac413ae00704cd76
Julie Murte <jmurte@e1z2r9p3.42.us.org>
2017-12-13 16:22:12 -0800

    making atoi returning 0 if some white non visile character is entered

--- ft_atoi.c
+++ ft_atoi.c
@@ -21,7 +21,7 @@ int		ft_atoi(const char *str)
 	sign = 1;
 	result = 0;
 	i = 0;
-	while (str[i] <= ' ')
+	while (str[i] == '\t' || str[i] == '\v' || str[i] == ' ')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
