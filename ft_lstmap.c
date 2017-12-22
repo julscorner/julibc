f6ff9f5bad8872549038d99725cb23cf3f592860
julscorner <livre5min@gmail.com>
2017-12-21 18:42:38 -0800

    update

---
+++ ft_lstmap.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmurte <jmurte@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/21 18:40:22 by jmurte            #+#    #+#             */
+/*   Updated: 2017/12/21 18:41:03 by jmurte           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*head;
+	t_list	*iter;
+
+	if (!lst || !(iter = ft_lstnew(NULL, 0)))
+		return (NULL);
+	iter = f(lst);
+	head = iter;
+	while (lst->next)
+	{
+		lst = lst->next;
+		iter->next = f(lst);
+		iter = iter->next;
+	}
+	return (head);
+}
