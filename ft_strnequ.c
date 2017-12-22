2cf74d123e6ba10a789fe37414d1883b616d0a1e
julscorner <livre5min@gmail.com>
2017-12-21 18:37:49 -0800

    update :)

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -17,24 +17,20 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (!s1 && !s2)
+	if ((!s1 && !s2) || !n)
 		return (1);
 	if (!s1 || !s2)
 		return (0);
 	while (i < n)
 	{
-		if ((*s1 == *s2) && (*s1 != '\0') && (*s2 != '\0'))
+		if ((*s1) == (*s2))
 		{
 			s1++;
 			s2++;
+			i++;
 		}
-		if ((*s1 == '\0') && (*s1 == *s2))
-			return (1);
-		i++;
+		else
+			return (0);
 	}
-	if ((*s1 == '\0') && (*s1 == *s2))
-		return (1);
-	if ((*s1 == '\0') || (*s1 == *s2))
-		return (1);
-	return (0);
+	return (1);
 }
