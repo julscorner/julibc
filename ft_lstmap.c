d86cc408b97e487ab5028f748bd1b959fa5e0367
julscorner <livre5min@gmail.com>
2017-12-23 12:15:46 -0800

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*head;
